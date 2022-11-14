e656ac474302dd0e06ec9d8b6420668ec3d2253f
Momoleng <mouyleng@Momolengs-MacBook-Pro.local>
2022-11-14 22:12:57 +0700

    Revert "update pov"
    This reverts commit c88c56bb89ce687c38e55a6187191a67feabf8c8.

--- components/skillsets.vue
+++ components/skillsets.vue
@@ -6,7 +6,10 @@
           {{ data.main.pointofview }}
         </div>
         <div class="font-normal text-sm text-indigo-900 pt-6 text-justify">
-          {{ data.main.subPOV }}
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consectetur
+          quia tempora optio expedita quam assumenda dolorum reprehenderit
+          repellat magnam quos sapiente quisquam perferendis nam facilis
+          molestias, esse earum aspernatur error?
         </div>
         <div></div>
       </div>
